9a29fa289c7dae836c2869da348d0f48c6807dfd
rasade88@gmail.com <rasade88@gmail.com@9c393139-2054-d63a-e521-a42ab57d0487>
2011-04-18 03:16:49 +0000

    Added login

---
+++ trunk/IntelliRadio/PHP/FrontEnd/login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+require_once('../dbAccess/dbAccess.inc');
+require_once('user.inc');
+$loginParam = $_SESSION['user']->loggedIn;
+    if($loginParam != true) { ?>
+
+<table border="0" align="center" cellpadding="5" cellspacing="0" bordercolor="#3366FF">
+    <tr>
+
+        <td>
+		
+
+            <form name="chooser" action="index.php?page=login&loginAttempt=true" method="POST">
+                <p align="center" class="main_nav">&nbsp;</p>
+                <p align="center"><span class="style3">Username</span> :
+                    <input type="text" name="username" />
+                </p>
+                <p align="center"><br/>
+                    <span class="style3">Password</span>       :
+                    <input type="password" name="password" />
+
+                </p>
+                <p align="right">
+                    <input type="submit" name="submit" id="button1" value="Login" />
+                </p>
+            </form>
+
+        </td>
+    </tr>
+</table>
+
+<?php } else {?>
+		<p>You are logged in</p>
+<?php } ?>
+
+<?php
+
+if($_GET['loginAttempt']==true) {
+
+   
+}
+
+?>
+
