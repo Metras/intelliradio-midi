f64a2e743e78ba0374603d959cd7a200b7eb165d
rasade88 <rasade88@gmail.com>
2011-04-23 01:42:38 +0000

    Request Class skeleton added

---
+++ IntelliRadio/PHP/FrontEnd/request.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 
+ * @desc Class to create request object for each request
+ *
+ * @author ramindu
+ *
+ */
+class Request {
+	/**
+	 * 
+	 * Initialize a request object
+	 */
+	function __construct() {
+		
+	}
+	/**
+	 * 
+	 * append latest request to relevant container playlist
+	 */
+	function appendRequest() {
+		
+	}
+	/**
+	 * 
+	 * Send updated container playlist to server
+	 */
+	function sendRequest() {
+		
+	}
+	/**
+	 * 
+	 * This is the function that gets called by the GUI
+	 * @param $trackId int Id of the requested track
+	 */
+	function requestTrack($trackId) {
+		
+	}
+}
