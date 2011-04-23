1b6e90e3c59d3d245de71c69d292ae8ea8526971
rasade88@gmail.com <rasade88@gmail.com@9c393139-2054-d63a-e521-a42ab57d0487>
2011-04-23 18:59:46 +0000

    Added functions to request class

--- trunk/IntelliRadio/PHP/FrontEnd/request.php
+++ trunk/IntelliRadio/PHP/FrontEnd/request.php
@@ -1,4 +1,5 @@
 <?php
+require_once('../includes/include_db.inc');
 /**
  * 
  * @desc Class to create request object for each request
@@ -9,10 +10,28 @@
 class Request {
 	/**
 	 * 
+	 * Name of container the requested track belongs to
+	 * @var String
+	 */
+	var $container_name;
+	/**
+	 * 
+	 * Id of requested track
+	 * @var int
+	 */
+	var $track_id;
+	var $track_name;
+	
+	/**
+	 * 
 	 * Initialize a request object
 	 */
-	function __construct() {
-		
+	function __construct($container_name, $track_id) {
+		$this->track_id = $track_id;
+		$this->container_name = $container_name;
+		$db = dbAccess::getInstance();
+		$db->setQuery("SELECT name FROM tracks WHERE id='{$this->track_id}'");
+		$this->track_name = $db->loadResult();
 	}
 	/**
 	 * 
@@ -26,7 +45,22 @@ class Request {
 	 * Send updated container playlist to server
 	 */
 	function sendRequest() {
+		define(HOST, '192.168.1.1');
+		define(PORT, '3998');
 		
+		set_time_limit(0);
+		$requestStream = $this->container_name.','.$this->track_name;
+		if ( !$requestSocket = socket_create(AF_INET, SOCK_STREAM, 0) ) {
+			echo '<p>Error: Could not create socket!</p>';
+		} 
+		if ( !socket_bind($socket, HOST, PORT) ) {
+			echo '<p>Error: Could not bind to port!</p>';
+		}
+		if ( !socket_write($requestSocket, $requestStream, strlen ($requestStream))) {
+			echo '<p>Error: Could not write request to socket!</p>';
+		}
+
+		socket_close($requestSocket);
 	}
 	/**
 	 * 
