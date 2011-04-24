8b12ba14082a591fb474822aade9251e9d8717ad
rasade88 <rasade88@gmail.com>
2011-04-24 06:19:43 +0000

    Commit

--- IntelliRadio/PHP/FrontEnd/loginchecker.php
+++ IntelliRadio/PHP/FrontEnd/loginchecker.php
@@ -2,7 +2,7 @@
 if ( is_null($_SESSION['user']) ) {
 	//header('Location:index.php');
 	echo "<script type=\"text/javascript\">"
-		 ."alert('You have not logged in');"
+		 ."alert('You have not logged in!');"
 		 ."self.location='index.php';"
 		 ."</script>";
 }
